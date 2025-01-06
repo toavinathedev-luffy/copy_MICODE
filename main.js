c31943c8ecf6bcf216343692329c4b5bc00219ef
toavina <toavinathefirst@gmail.com>
2025-01-06 14:40:29 +0300

    button submit en alert , liste des reponses et question num 1

--- main.js
+++ main.js
@@ -1,76 +1,70 @@
 import "./style.css";
 import { Questions } from "./questions";
-const app = document.querySelector(/*selecteur CSS*/ "#app");
-const startButton = document.querySelector("#start");
-
-//Affichage de la question
-const getTitleOfQuestion = (questionTitle) => {
-  const title = document.createElement("h3");
-  title.innerText = questionTitle;
-  return title;
-};
-
+const app = document.getElementById("app");
+const startButton = document.getElementById("start");
 function startQuiz(event) {
-  event.stopPropagation(); //Arrete la propagation de l event
+  console.log("start Clicked");
   let currentQuestion = 0;
   let score = 0;
-  const createAnswer = (answers) => {
-    const answersDiv = document.createElement("div");
-    answersDiv.classList.add("answers");
-    for (const answer of answers) {
-      const getAnswerElement = (text = "") => {
-        const label = document.createElement("label");
-        label.innerText = text;
-        const input = document.createElement("input");
-        const inputId = text.replaceAll(" ", "_").toLowerCase();
-        input.id = inputId;
-        label.htmlFor = inputId;
-        input.setAttribute("type", "radio");
-        input.setAttribute("name", "answer");
-        input.setAttribute("value", text);
-        label.appendChild(input);
-        return label;
-      };
-      const label = getAnswerElement(answer);
-      answersDiv.appendChild(label);
-    }
-    return answersDiv;
-  };
   const clean = () => {
     while (app.firstElementChild) {
       app.firstElementChild.remove();
     }
   };
-  const displayQuestion = (indexOfQuestion) => {
-    const question = Questions[indexOfQuestion];
-    if (!question) {
-      //finish Quiz
-    }
-    const title = getTitleOfQuestion(question.question);
-    app.appendChild(title);
-    const listOFAnswer = createAnswer(question.answers);
-    app.appendChild(listOFAnswer);
-
-    const submitButton = getSubmitButton();
-    function submitAnswerOfUser(e) {
-      e.stopPropagation();
-      const selectedAnswer = app.querySelector("input[name='answer']:checked");
-      const value = selectedAnswer ? selectedAnswer.value : "";
-      const question = Questions[currentQuestion];
-      const isCorrect = question.correct === value;
-      alert(`${isCorrect ? "correct" : "incorrect"}`);
-    }
-    app.appendChild(submitButton);
-    submitButton.addEventListener("click", submitAnswerOfUser);
-  };
   clean();
+  const displayQuestion = (questionIndex) => {
+    const question = Questions[questionIndex];
+    if (!question) {
+      //finish quiz
+    }
+    const title = getTitleElement(question.question);
+    app.appendChild(title);
+    const answersDiv = createAnswers(question.answers);
+    app.appendChild(answersDiv);
+    const submitButton = getSubmitButton();
+    submitButton.addEventListener("click", submit);
+    app.appendChild(submitButton);
+  };
   displayQuestion(currentQuestion);
+  function createAnswers(answers) {
+    const answersDiv = document.createElement("div");
+    answersDiv.classList.add("answers");
+    for (const answer of answers) {
+      const label = getAnswerElement(answer);
+      answersDiv.appendChild(label);
+    }
+    return answersDiv;
+  }
 }
-
 startButton.addEventListener("click", startQuiz);
 
+function submit() {
+  //gerer la submit
+  const selectedAnswer = app.querySelector(`input[name="answer"]:checked`);
+  const value = selectedAnswer.value;
+  alert(`tu as répondu ${value}`);
+}
+function getTitleElement(titleOfTheQuestion) {
+  const title = document.createElement("h3");
+  title.innerText = titleOfTheQuestion;
+  return title;
+}
+function getAnswerElement(answer) {
+  const label = document.createElement("label");
+  label.innerText = answer;
+  const input = document.createElement("input");
+  let inputId = "";
+  inputId = answer.replaceAll(" ", "-").toLowerCase();
+  input.id = inputId;
+  label.htmlFor = inputId;
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "answer");
+  input.setAttribute("value", answer);
+  label.appendChild(input);
+  return label;
+}
 function getSubmitButton() {
-  const submitButton = document.createElement("button");
-  submitButton.innerText = "Submit";
-  return submitButton;
+  const submit = document.createElement("button");
+  submit.innerText = "Submit";
+  return submit;
 }
